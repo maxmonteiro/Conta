4eea7c8e8a4b87692c999fa2e0a202b9c4467eb4
Maximo <maxmonteiro86@gmail.com>
2017-03-16 11:00:49 -0300

    teste

--- Conta/conta/src/conta/TestaDuasContas.java
+++ Conta/conta/src/conta/TestaDuasContas.java
@@ -5,10 +5,10 @@ public class TestaDuasContas {
 	public static void main(String[] args) {
 		
 		Conta c1 = new Conta();
-		c1.deposita(100);
+		c1.deposita(200);
 		
 		Conta c2 = new Conta();
-		c2.deposita(300);
+		c2.deposita(400);
 		
 		System.out.println(c1.saldo);
 		System.out.println(c2.saldo);
